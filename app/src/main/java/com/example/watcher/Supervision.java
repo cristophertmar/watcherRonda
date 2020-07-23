57679307f160e01f59e4505766e8163997d346ff
Cristopher Torres <cristopher.tmar@gmail.com>
2020-07-23 09:51:08 -0500

    Primer commit

---
+++ app/src/main/java/com/example/watcher/Supervision.java
@@ -0,0 +1,14 @@
+package com.example.watcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Supervision extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_supervision );
+    }
+}
